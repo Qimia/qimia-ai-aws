b801b759a89b0b9709ac0bbbdd77fb642e401ed7
Saba Fallah <s.fallah@qimia.de>
2023-05-31 15:15:23 +0200

    refactored

--- ci_cd/init_terraform.sh
+++ ci_cd/init_terraform.sh
@@ -2,5 +2,5 @@ echo 'This script needs to be run with a single argument that is the environment
 cat ~/.aws/config
 cd cloud-resources
 env="$1"
-terraform init -backend-config=bucket="$env-qimia-tf-state"
+terraform init -backend-config="$env.tfbackend"
 cd ..
