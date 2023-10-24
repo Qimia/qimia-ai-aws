84b41667107a6507fd7bcbe5a781067f9dc919b5
Oguz Uzman <o.uzman@qimia.de>
2023-10-24 17:13:11 +0200

    chore: automate admin password setting

--- ci_cd/deploy-secrets.sh
+++ ci_cd/deploy-secrets.sh
@@ -8,7 +8,7 @@ put_secret () {
   FIELD_VALUE=$2
   [ ! -z "$FIELD_VALUE" ] && echo "Putting $FIELD_NAME" || return
   secret_full_arn=$(aws secretsmanager describe-secret --secret-id "arn:aws:secretsmanager:$REGION:$AWS_ACCOUNT_ID:secret:/qimia-ai/$ENV/$FIELD_NAME" --query 'ARN' --output text)
-  echo "Putting the value to secret $secret_full_arn"
+  echo "Putting the value '$FIELD_VALUE' to secret $secret_full_arn"
   res=$(aws secretsmanager put-secret-value --secret-id "$secret_full_arn" --secret-string "$FIELD_VALUE")
   echo $res
 }
@@ -16,3 +16,5 @@ put_secret () {
 put_secret "email_password" "$EMAIL_PASSWORD"
 put_secret "email_address" "$EMAIL_ADDRESS"
 put_secret "email_smtp_send_address" "$SMTP_SEND_ADDRESS"
+put_secret "admin_email_address" "$ADMIN_EMAIL"
+put_secret "admin_email_password" "$ADMIN_PASSWORD"
